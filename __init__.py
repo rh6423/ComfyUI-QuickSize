0f021a3c54a6726db2be32516f470b7c941ef8bf
rh6423 <45771723+rh6423@users.noreply.github.com>
2025-11-05 17:46:42 -0700

    updated init

--- __init__.py
+++ __init__.py
@@ -6,9 +6,13 @@ from .src.quicksize import (
     NODE_DISPLAY_NAME_MAPPINGS as _BASE_NAMES,
 )
 
+WEB_DIRECTORY = "./web"
+
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
-
 NODE_CLASS_MAPPINGS.update(_BASE_NODES)
 NODE_DISPLAY_NAME_MAPPINGS.update(_BASE_NAMES)
+
+# If you export __all__, include WEB_DIRECTORY so Comfy detects it:
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
 
